ddeeb9718820fc76397008fcd3ab5b3f39f46034
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-02 16:47:08 +0200

    create the text element in the main screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-import './App.css'
-import Header from './components/Header/Header'
-import mainScreenImg from '/main screen img.png'
-function App() {
+import './App.css';
+import Header from './components/Header/Header';
+import mainScreenImg from '/main screen img.png';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+library.add(faPlay);
+
+function App() {
   return (
     <>
-    <div className="wrap">
-    <Header />
-    <div className="mainScreen">
-       <div className="mainScreenDecoration"></div>
-       <img src={mainScreenImg} alt="mainScreenImg" className='mainScreenImg' />
-    </div>
-    </div>
+      <div className="wrap">
+        <Header />
+        <div className="mainScreen">
+          <div className="mainScreenDecoration"></div>
+          <img src={mainScreenImg} alt="mainScreenImg" className="mainScreenImg" />
+          <div className="mainScreen_text">
+            <div className="title">
+              We Care About Your <br /> Dental Health.
+            </div>
+            <div className="subtitle">
+              Dental or oral health is concerned with your teeth, gums, and mouth. A healthy mouth is free of infections, injuries, and other problems.
+            </div>
+            <div className="button_video_con">
+              <div className="mainScreen_btn">Appointment</div>
+              <div className="watchVideo_btn">
+                <div className="watchVideoIcon">
+                  <FontAwesomeIcon icon={faPlay} />
+                </div>
+                Watch Video
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
