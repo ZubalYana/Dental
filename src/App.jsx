9b63c73f497028f8d4ddb593efaf19125c515034
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-02 16:41:44 +0200

    install, place and style the main page img

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header/Header'
+import mainScreenImg from '/main screen img.png'
 function App() {
 
   return (
@@ -8,6 +9,7 @@ function App() {
     <Header />
     <div className="mainScreen">
        <div className="mainScreenDecoration"></div>
+       <img src={mainScreenImg} alt="mainScreenImg" className='mainScreenImg' />
     </div>
     </div>
     </>
