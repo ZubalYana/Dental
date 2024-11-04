51364cb53d52bb9666b8b61f7a93074b515f08c4
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-04 05:46:16 +0200

    create a hover effect on the read more btn and finish the servises screen

--- src/components/servicesCard/servicesCard.jsx
+++ src/components/servicesCard/servicesCard.jsx
@@ -6,15 +6,16 @@ const ServicesCard = (props) => {
     return (
         <div className="servicesCard">
             <div className="servicesCard_img">
-                <img src={props.img} alt="" />
+                <img src={props.img} alt="teethImg" className="servicesCard_img" />
             </div>
             <div className="servicesCard_text">
                 <h3>{props.title}</h3>
                 <p>​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​​Dental or oral health is concerned with your teeth, gums and mouth. healthy mouth, free of infections. </p>
             </div>
             <div className="readMore">
-                <div className="buttonText">Read More <div className="readMore_underline"></div></div>
-                <img src={arrowRightSvg} alt="" />
+                <div className="buttonText">Read More <img src={arrowRightSvg} alt="" />
+                </div>
+                <div className="readMore_underline"></div>
             </div>
         </div>
     )
