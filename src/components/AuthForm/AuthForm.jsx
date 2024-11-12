aa2a3cffc9f8c427fe8b525db11ec0ef643ab905
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-12 16:48:04 +0200

    style the registration popup

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
+import './AuthForm.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function AuthForm() {
   return (
-    <div className='authForm'>
+    <div className="authFormCon">
+            <div className='authForm'>
+            <FontAwesomeIcon icon={faXmark} id='authForm_xmark' />
+
       <div className="registrationForm">
         <h2>Registration</h2>
         <input type="text" placeholder='Name' id='registration_name' />
         <input type="email" placeholder='Email' id='registration_email' />
         <div className="passwordCon">
           <input type="password" placeholder='Password' id='registration_password' />
-          <FontAwesomeIcon icon={faEye} />
+          <FontAwesomeIcon icon={faEye} className='password_eye' />
         </div>
         <button id='registerBtn'>Register</button>
-        <p>Already have an account?</p>
+        <p id='alreadyHaveAnAccount'>Already have an account?</p>
       </div>
       <div className="logInForm">
         <h2>Log in</h2>
@@ -24,5 +29,7 @@ export default function AuthForm() {
         <p>Don't have an account yet?</p>
       </div>
     </div>
+    </div>
+
   );
 }
