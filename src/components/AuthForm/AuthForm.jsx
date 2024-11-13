7129ed1d2f391b8e9dabd36f920bedc81022eeef
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-13 18:02:48 +0200

    fix the registration, complete sending the data in the database

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './AuthForm.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 export default function AuthForm({ isFormOpen, formType, closeForm, setFormType }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await axios.post('http://localhost:3000/register', { name, email, password });
+        console.log('User registered successfully:', response);
+        setName('');
+        setEmail('');
+        setPassword('');
+    } catch (error) {
+        console.log('Error registering user:', error);
+    }
+};
+
+  const handleLogin = async () => {
+    try {
+        const response = await axios.post('http://localhost:3000/login', { email, password });
+        console.log("logged in");
+        const token = response.data.token;
+        setToken(token);
+        localStorage.setItem('token', token);
+    } catch (err) {
+        console.log('User not found');
+        setToken(null);
+        localStorage.setItem('token', null);
+    }
+};
+
   return (
     <div
       className="authFormCon"
@@ -17,13 +49,32 @@ export default function AuthForm({ isFormOpen, formType, closeForm, setFormType 
         {formType === 'signup' ? (
           <div className="registrationForm">
             <h2>Registration</h2>
-            <input type="text" placeholder='Name' id='registration_name' />
-            <input type="email" placeholder='Email' id='registration_email' />
-            <div className="passwordCon">
-              <input type="password" placeholder='Password' id='registration_password' />
-              <FontAwesomeIcon icon={faEye} className='password_eye' />
-            </div>
-            <button id='registerBtn'>Register</button>
+<input
+  type="text"
+  placeholder="Name"
+  id="registration_name"
+  value={name}
+  onChange={(e) => setName(e.target.value)}
+/>
+<input
+  type="text"
+  placeholder="Email"
+  id="registration_email"
+  value={email}
+  onChange={(e) => setEmail(e.target.value)}
+/>
+<div className="passwordCon">
+  <input
+    type="password"
+    placeholder="Password"
+    id="registration_password"
+    value={password}
+    onChange={(e) => setPassword(e.target.value)}
+  />
+  <FontAwesomeIcon icon={faEye} className="password_eye" />
+</div>
+
+            <button id="registerBtn" onClick={handleSubmit}>Register</button>
             <p 
               id='alreadyHaveAnAccount' 
               onClick={() => setFormType('login')}
@@ -36,7 +87,7 @@ export default function AuthForm({ isFormOpen, formType, closeForm, setFormType 
             <h2>Log in</h2>
             <input type="text" placeholder='Email' id='logInEmail' />
             <input type="password" placeholder='Password' id='logInPassword' />
-            <button id='logInBtn'>Log in</button>
+            <button id="logInBtn" onClick={handleLogin}>Log in</button>
             <p 
               id='doNotHaveAnAccount' 
               onClick={() => setFormType('signup')}
