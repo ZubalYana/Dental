726da24e8b73d969566f1d2eb30bcc193d7cc2d3
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-22 07:49:19 +0200

    fill feedback with required fields

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
 export default function Feedback() {
+  const [feedback, setFeedback] = useState({status: false})
   return (
-    <div>Feedback</div>
+    <div>
+      <h2>Feedback</h2>
+      <input type="text" placeholder='Name' id='name' value={feedback.name} onChange={(e) => setFeedback({ ...feedback, name: e.target.value })} />
+      <textarea name="" id="feedback" placeholder='Feedback' value={feedback.feedback} onChange={(e) => setFeedback({ ...feedback, feedback: e.target.value })}></textarea>
+      <select name="rating" id="rating" value={feedback.rating} onChange={(e) => setFeedback({ ...feedback, rating: parseInt(e.target.value) })}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button onClick={() => {
+        alert('Feedback sent')
+        axios.post('http://localhost:3000/feedback', feedback
+        )}}>Send</button>
+    </div>
   )
 }
