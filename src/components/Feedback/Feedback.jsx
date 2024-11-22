9291943b9e9561278602c4f017d1c615423f9654
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-22 09:17:48 +0200

    style the feedback fields and style their positions

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import './Feedback.css'
 export default function Feedback() {
   const [feedback, setFeedback] = useState({status: false})
   return (
-    <div>
+    <div className='feedbackContainer'>
       <h2>Feedback</h2>
+      <div className="nameRatingCon">
       <input type="text" placeholder='Name' id='name' value={feedback.name} onChange={(e) => setFeedback({ ...feedback, name: e.target.value })} />
-      <textarea name="" id="feedback" placeholder='Feedback' value={feedback.feedback} onChange={(e) => setFeedback({ ...feedback, feedback: e.target.value })}></textarea>
       <select name="rating" id="rating" value={feedback.rating} onChange={(e) => setFeedback({ ...feedback, rating: parseInt(e.target.value) })}>
         <option value="1">1</option>
         <option value="2">2</option>
@@ -14,7 +15,11 @@ export default function Feedback() {
         <option value="4">4</option>
         <option value="5">5</option>
       </select>
-      <button onClick={() => {
+      </div>
+
+      <textarea name="" id="feedback" placeholder='Feedback' value={feedback.feedback} onChange={(e) => setFeedback({ ...feedback, feedback: e.target.value })}></textarea>
+
+      <button className='feedback_btn' onClick={() => {
         alert('Feedback sent')
         axios.post('http://localhost:3000/feedback', feedback
         )}}>Send</button>
