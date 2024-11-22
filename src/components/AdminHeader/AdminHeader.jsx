e5fcc03a37647420077b7ccb1525a65d0972cb2c
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-22 08:05:30 +0200

    create and style the admin panel header

---
+++ src/components/AdminHeader/AdminHeader.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './AdminHeader.css'
+import logoSvg from '../../assets/logo.svg'
+export default function AdminHeader() {
+  return (
+    <>
+    <header>
+    <div className="logo">
+        <img src={logoSvg} alt="logo" className="logoSvg"/>
+        Dental Admin
+    </div>
+    <li className='nav'>
+        <li className='nav_item' id='feedback_li'>Feedback<div className="animatedLine"></div></li>
+        <li className='nav_item' id='doctors_li'>Doctors<div className="animatedLine"></div></li>
+        <li className='nav_item' id='newsletter_li'>NewsLetter<div className="animatedLine"></div></li>
+        <li className='nav_item' id='appointments_li'>Appointments<div className="animatedLine"></div></li>
+    </li>
+    </header>
+
+    </>
+  )
+}
--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './AdminPanel.css'
 import Feedback from '../Feedback/Feedback'
-
+import AdminHeader from '../AdminHeader/AdminHeader'
 export default function AdminPanel() {
   return (
     <>
+    <div className="wrap">
+      <AdminHeader />
       <Feedback />
+    </div>
     </>
   )
 }
