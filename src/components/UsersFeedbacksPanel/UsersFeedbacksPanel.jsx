c937eeca2c9fb7392a55b4c3db454d1b203d64ea
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-26 06:21:20 +0200

    get the feedbacks in the admin panel console and fix the database mistakes

--- src/components/UsersFeedbacksPanel/UsersFeedbacksPanel.jsx
+++ src/components/UsersFeedbacksPanel/UsersFeedbacksPanel.jsx
@@ -1,23 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './UsersFeedbacksPanel.css'
 import axios from 'axios'
 export default function UsersFeedbacksPanel() {
-    axios.get('/api/feedbacks').then(res => {
-        console.log(res)
-        // const feedbacks = res.data
-        // console.log(feedbacks)
-        // const usersFeedbacksCon = document.getElementById('usersFeedbacksCon')
-        // usersFeedbacksCon.innerHTML = ''
-        // feedbacks.map(feedback => {
-        //     const userFeedback = document.createElement('div')
-        //     userFeedback.className = 'userFeedback'
-        //     userFeedback.innerHTML = `
-        //     <div class="userFeedback_name">${feedback.name}</div>
-        //     <div class="userFeedback_rating">${feedback.rating}</div>
-        //     <div class="userFeedback_feedback">${feedback.feedback}</div>`
-        //     usersFeedbacksCon.appendChild(userFeedback)
-        // })
+  const [reviewsToCheck, setReviewsToCheck] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/feedbacks').then(res => { 
+        setReviewsToCheck(res.data);
+        console.log(res.data);
     })
+  }, [])
+
+  console.log(reviewsToCheck)
+
   return (
     <>
     <div id="usersFeedbacksCon"></div>
