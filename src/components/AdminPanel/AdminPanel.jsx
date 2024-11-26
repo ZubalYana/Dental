d93786e9a3d228420471d192ed2cbdfa7b851ec9
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-26 16:01:02 +0200

    display the reviews in the admin panel

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
-import './AdminPanel.css'
-import Feedback from '../Feedback/Feedback'
-import AdminHeader from '../AdminHeader/AdminHeader'
-import UsersFeedbacksPanel from '../UsersFeedbacksPanel/UsersFeedbacksPanel'
+import React, { useEffect, useState } from 'react';
+import './AdminPanel.css';
+import Feedback from '../Feedback/Feedback';
+import AdminHeader from '../AdminHeader/AdminHeader';
+import axios from 'axios';
+
 export default function AdminPanel() {
+  const [reviewsToCheck, setReviewsToCheck] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/feedbacks').then((res) => {
+      const pendingReviews = res.data.filter((review) => !review.accepted);
+      setReviewsToCheck(pendingReviews);
+    });
+  }, []);
+
   return (
     <>
-    <div className="wrap">
-      <AdminHeader />
-      <div className="feedbackScreen">
-      <UsersFeedbacksPanel />
+      <div className="wrap">
+        <AdminHeader />
+        <div className="feedbackScreen">
+          <div className="feedbacksToCheckCon">
+            {reviewsToCheck.map(({ id, name, rating, img, feedback }) => (
+              <div key={id}>
+                <h2>{name}</h2>
+                <p>{rating}</p>
+                <p>{feedback}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
