99b1a1bec4aaa0ccea312850b300dcf6a98c8cee
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-11-27 05:48:03 +0200

    add&style the accept and reject btns

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './AdminPanel.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons';
-import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';  
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import defaultUserPic from "/default user picture.png";
 import AdminHeader from '../AdminHeader/AdminHeader';
 import axios from 'axios';
@@ -43,6 +45,10 @@ export default function AdminPanel() {
                     alt="Reviewer" 
                     className="reviewerImg" 
                   />
+                  <div className="reviewActions">
+                    <div id="rejectReview" className='reviewAction'><FontAwesomeIcon icon={faXmark}/></div>
+                    <div id="acceptReview" className='reviewAction'><FontAwesomeIcon icon={faCheck}/></div>
+                  </div>
                   <div className="reviewerName">{name}</div>
                   <div className="reviewerText">{feedback}</div>
                   <div className="stars">{renderStars(rating)}</div>
